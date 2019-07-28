58df3403f5fd3cfa323c64c8a59c96fabac0f069
Nzix <iminezn5656@outlook.com>
2019-07-28 23:39:49 +0800

    remove netease from default match order #161

--- provider/match.js
+++ provider/match.js
@@ -14,7 +14,7 @@ const provider = {
 
 const match = (id, source) => {
 	let meta = {}
-	let candidate = (source || global.source || ['netease', 'qq', 'xiami', 'baidu']).filter(name => name in provider)
+	let candidate = (source || global.source || ['qq', 'xiami', 'baidu']).filter(name => name in provider)
 	return find(id)
 	.then(info => {
 		meta = info
