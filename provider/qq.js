47afb172e4eae562daeb688c0a0e41537a7072ae
Nzix <iminezn5656@outlook.com>
2019-08-27 19:05:41 +0800

    sip resources are precious

--- provider/qq.js
+++ provider/qq.js
@@ -19,11 +19,11 @@ const playable = song => {
 const search = info => {
 	let url =
 		'https://c.y.qq.com/soso/fcgi-bin/client_search_cp?' +
-		'ct=24&qqmusic_ver=1298&new_json=1&remoteplace=txt.yqq.center' +
-		'&searchid=46804741196796149&t=0&aggr=1&cr=1&catZhida=1&lossless=0' +
-		'&flag_qc=0&p=1&n=20&w=' + encodeURIComponent(info.keyword) +
-		'&g_tk=5381&jsonpCallback=MusicJsonCallback10005317669353331&loginUin=0&hostUin=0' +
-		'&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0'
+		'ct=24&qqmusic_ver=1298&new_json=1&remoteplace=txt.yqq.center&' +
+		'searchid=46804741196796149&t=0&aggr=1&cr=1&catZhida=1&lossless=0&' +
+		'flag_qc=0&p=1&n=20&w=' + encodeURIComponent(info.keyword) + '&' +
+		'g_tk=5381&jsonpCallback=MusicJsonCallback10005317669353331&loginUin=0&hostUin=0&' +
+		'format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0'
 
 	return request('GET', url)
 	.then(response => response.jsonp())
@@ -36,9 +36,9 @@ const search = info => {
 	})
 }
 
-const ticket = () => {
+const ticket = id => {
 	const classic = ['001yS0N33yPm1B', '000bog5B2DYgHN', '002bongo1BDtKz', '004RDW5Q2ol2jj', '001oEME64eXNbp', '001e9dH11YeXGp', '0021onBk2QNjBu', '001YoUs11jvsIK', '000SNxc91Mw3UQ', '002k94ea4379uy']
-	const id = classic[Math.floor(classic.length * Math.random())]
+	id = id || classic[Math.floor(classic.length * Math.random())]
 
 	// let url =
 	// 	'https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg' +
@@ -90,10 +90,7 @@ const ticket = () => {
 		let vkey = 
 			jsonBody.req_0.data.midurlinfo[0].vkey ||
 			(jsonBody.req_0.data.testfile2g.match(/vkey=(\w+)/) || [])[1]
-		if(vkey)
-			return vkey
-		else
-			return Promise.reject()
+		return vkey || Promise.reject()
 	})
 	.catch(() => insure().qq.ticket())
 }
@@ -101,7 +98,7 @@ const ticket = () => {
 const track = id => {
 	return cache(ticket)
 	.then(vkey => {
-		let host = ['streamoc.music.tc.qq.com', 'isure.stream.qqmusic.qq.com', 'dl.stream.qqmusic.qq.com', 'aqqmusic.tc.qq.com/amobile.music.tc.qq.com'][1]
+		let host = ['streamoc.music.tc.qq.com', 'mobileoc.music.tc.qq.com', 'isure.stream.qqmusic.qq.com', 'dl.stream.qqmusic.qq.com', 'aqqmusic.tc.qq.com/amobile.music.tc.qq.com'][4]
 		let songUrl =
 			'http://' + host + '/M500' + id +
 			'.mp3?vkey=' + vkey +
@@ -123,6 +120,20 @@ const track = id => {
 	// 	let xml = require('zlib').inflateSync(body.slice(5)).toString()
 	// 	let focus = xml.match(/<item name="(.+)">(.+)<\/item>/)
 	// 	return `http://streamoc.music.tc.qq.com/${focus[1]}?vkey=${focus[2]}&guid=0&uin=12345678&fromtag=6`
+	// })
+
+	// let url =
+	// 	'https://i.y.qq.com/v8/playsong.html?ADTAG=newyqq.song&songmid=' + id
+
+	// let mobile = {'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'}
+	// return request('GET', url, mobile)
+	// .then(response => response.body())
+	// .then(body => {
+	// 	let audio = body.match(/<audio[^>]+src="([^"]+)"[^>]*>/)
+	// 	if(audio)
+	// 		return audio[1].replace(/C400(\w+)\.m4a/, 'M500$1.mp3')
+	// 	else
+	// 		return Promise.reject()
 	// })
 }
 
