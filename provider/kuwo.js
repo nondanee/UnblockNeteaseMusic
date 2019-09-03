a4ca62dd44c4b0c0a76e73613c2f6c4d12784aa5
Nzix <iminezn5656@outlook.com>
2019-09-03 16:30:44 +0800

    change kuwo search api #193

--- provider/kuwo.js
+++ provider/kuwo.js
@@ -3,29 +3,43 @@ const insure = require('./insure')
 const request = require('../request')
 
 const search = info => {
+	// let url =
+	// 	// 'http://search.kuwo.cn/r.s?' +
+	// 	// 'ft=music&itemset=web_2013&client=kt&' +
+	// 	// 'rformat=json&encoding=utf8&' +
+	// 	// 'all=' + encodeURIComponent(info.keyword) + '&pn=0&rn=20'
+	// 	'http://search.kuwo.cn/r.s?' +
+	// 	'ft=music&rformat=json&encoding=utf8&' +
+	// 	'rn=8&callback=song&vipver=MUSIC_8.0.3.1&' +
+	// 	'SONGNAME=' + encodeURIComponent(info.name) + '&' +
+	// 	'ARTIST=' + encodeURIComponent(info.artists[0].name)
+
+	// return request('GET', url)
+	// .then(response => response.body())
+	// .then(body => {
+	// 	let jsonBody = eval(
+	// 		'(' + body
+	// 		.replace(/\n/g, '')
+	// 		.match(/try\s*\{[^=]+=\s*(.+?)\s*\}\s*catch/)[1]
+	// 		.replace(/;\s*song\s*\(.+\)\s*;\s*/, '') + ')'
+	// 	)
+	// 	let matched = jsonBody.abslist[0]
+	// 	if(matched)
+	// 		return matched.MUSICRID.split('_').pop()
+	// 	else
+	// 		return Promise.reject()
+	// })
+
 	let url =
-		// 'http://search.kuwo.cn/r.s?' +
-		// 'ft=music&itemset=web_2013&client=kt&' +
-		// 'rformat=json&encoding=utf8&' +
-		// 'all=' + encodeURIComponent(info.keyword) + '&pn=0&rn=20'
-		'http://search.kuwo.cn/r.s?' +
-		'ft=music&rformat=json&encoding=utf8&' +
-		'rn=8&callback=song&vipver=MUSIC_8.0.3.1&' +
-		'SONGNAME=' + encodeURIComponent(info.name) + '&' +
-		'ARTIST=' + encodeURIComponent(info.artists[0].name)
+		'http://www.kuwo.cn/api/www/search/searchMusicBykeyWord?' +
+		'key=' + encodeURIComponent(info.keyword) + '&pn=1&rn=30'
 
 	return request('GET', url)
-	.then(response => response.body())
-	.then(body => {
-		let jsonBody = eval(
-			'(' + body
-			.replace(/\n/g, '')
-			.match(/try\s*\{[^=]+=\s*(.+?)\s*\}\s*catch/)[1]
-			.replace(/;\s*song\s*\(.+\)\s*;\s*/, '') + ')'
-		)
-		let matched = jsonBody.abslist[0]
+	.then(response => response.json())
+	.then(jsonBody => {
+		let matched = jsonBody.data.list[0]
 		if(matched)
-			return matched.MUSICRID.split('_').pop()
+			return matched.musicrid.split('_').pop()
 		else
 			return Promise.reject()
 	})
