f5e78b1d0c6e72eb49ce08d7a2112554123f5b63
binsee <binsee@163.com>
2020-03-25 20:41:09 +0800

    Revert "fix mini program"
    This reverts commit 7fbb243c27f4f2c7c8ba6cb3051ce118c7b8551f.

--- src/hook.js
+++ src/hook.js
@@ -83,7 +83,6 @@ hook.request.before = ctx => {
 			if ('x-napm-retry' in req.headers) delete req.headers['x-napm-retry']
 			req.headers['X-Real-IP'] = '118.88.88.88'
 			if (req.url.includes('stream')) return // look living eapi can not be decrypted
-			if (req.url.includes('miniprogram')) return // miniprogram eapi can not be decrypted
 			if (body) {
 				let data = null
 				const netease = {}
