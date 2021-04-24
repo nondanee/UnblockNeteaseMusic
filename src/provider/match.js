132660e83514b2ef74e91aa7f017e219f9a3e9ae
Tianling Shen <cnsztl@gmail.com>
2021-04-24 14:26:48 +0800

    provider/match: add md5 key for kuwo
    Signed-off-by: Tianling Shen <cnsztl@gmail.com>

--- src/provider/match.js
+++ src/provider/match.js
@@ -51,7 +51,7 @@ const check = url => {
 			song.md5 = url.split('/').slice(-1)[0].replace(/\..*/g,'')
 		else if (url.includes('qq.com'))
 			song.md5 = response.headers['server-md5']
-		else if (url.includes('xiami.net') || url.includes('qianqian.com'))
+		else if (url.includes('xiami.net') || url.includes('qianqian.com') || url.includes('kuwo.cn'))
 			song.md5 = response.headers['etag'].replace(/"/g, '').toLowerCase()
 		song.size = parseInt((response.headers['content-range'] || '').split('/').pop() || response.headers['content-length']) || 0
 		song.url = response.url.href
