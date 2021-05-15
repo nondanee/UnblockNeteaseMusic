nondanee/UnblockNeteaseMusic

commit 128bb941e3c40cb6798132bf4c2b67a8b74ff0f9

Revert "provider/match: add md5 key for kuwo"
However the value kuwo returned is not the correct md5 key in some cases.

This reverts commit 132660e83514b2ef74e91aa7f017e219f9a3e9ae.

Signed-off-by: Tianling Shen <cnsztl@gmail.com>

src/provider/match.js
@@ -50,7 +50,7 @@ const check = url => {
 			song.md5 = url.split('/').slice(-1)[0].replace(/\..*/g,'')
 		else if (url.includes('qq.com'))
 			song.md5 = response.headers['server-md5']
-		else if (url.includes('qianqian.com') || url.includes('kuwo.cn'))
+		else if (url.includes('qianqian.com')
 			song.md5 = response.headers['etag'].replace(/"/g, '').toLowerCase()
 		song.size = parseInt((response.headers['content-range'] || '').split('/').pop() || response.headers['content-length']) || 0
 		song.url = response.url.href

commit 8f061bd84eef06ee0073c088b05db841578b04bd

fix(typo): add missing symbol to provider/match
Fixes: 128bb941e3c40c ("Revert "provider/match: add md5 key for kuwo"")

src/provider/match.js
@@ -50,7 +50,7 @@ const check = url => {
 			song.md5 = url.split('/').slice(-1)[0].replace(/\..*/g,'')
 		else if (url.includes('qq.com'))
 			song.md5 = response.headers['server-md5']
-		else if (url.includes('qianqian.com')
+		else if (url.includes('qianqian.com'))
 			song.md5 = response.headers['etag'].replace(/"/g, '').toLowerCase()
 		song.size = parseInt((response.headers['content-range'] || '').split('/').pop() || response.headers['content-length']) || 0
 		song.url = response.url.href