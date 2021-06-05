32aa83e28e28d57a45092739247254066943cb52
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-05 16:12:27 +0800

    style(polyfills): update to use the official unminified version

--- src/polyfills.js
+++ src/polyfills.js
@@ -6,20 +6,17 @@
  * The polyfill of Promise.any.
  *
  * commit   e1e75a1410490906d2da1bda0d4aafb1788f2195
- * file     min.js
+ * file     index.js
  *
  * Thanks to https://github.com/ungap/promise-any
  */
-
-
-Promise.any = (Promise.any || function (n) {
-	return new Promise(function (e, o, i, t) {
-		i = [], t = n.map(function (n, r) {
-			return Promise.resolve(n).then(e, function (n) {
-				return i[r] = n, --t || o({
-					errors: i
-				})
-			})
-		}).length
-	})
+Promise.any = (Promise.any || function ($) {
+	return new Promise(function (D, E, A, L) {
+		A = [];
+		L = $.map(function ($, i) {
+			return Promise.resolve($).then(D, function (O) {
+				return ((A[i] = O), --L) || E({ errors: A });
+			});
+		}).length;
+	});
 }).bind(Promise);
