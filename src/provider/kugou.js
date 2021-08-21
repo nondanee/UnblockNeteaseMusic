nondanee/UnblockNeteaseMusic

commit 6e06698d06491700c3dd6b13bc7557e0571e5850

refactor: Resolve a lot of warning from CodeQL and WebStorm (#115)
* fix: security issues

CWE-20: Incomplete URL substring sanitization
CWE-20: Incomplete regular expression for hostnames
CWE-400, CWE-730, CWE-1333: Polynomial regular expression used on uncontrolled data

* fix(editorconfig): remove some invalid property value reported by WebStorm

* fix(provider/kugou): don't implicitly declare a global JavaScript variable

* fix(providers/kwDNS): Invalid number of arguments, expected 1..3

* fix(providers/kwDNS): Variable initializer is redundant

* fix(providers/pyncmd): Argument type unknown is not assignable to parameter type string

* deps(long): add long for backward compatibility in kwDES.js

* refactor(providers/find): remove comma expression for maintainability

* refactor(kwDES): remove comma expression for maintainability

* refactor(provider/youtube): remove comma expression for maintainability

* refactor: replace == to ===

* refactor: Variable initializer is redundant

* refactor(kwDES): Variable initializer is redundant

** need to check if this change can work.

* fix(server): Variable might not have been initialized

* refactor: Unused constant & parameter

* fix(hook): Reserved word "package" used as name

* refactor(provider/find): Redundant character escape '\)' in RegExp

* refactor(cli): Consider shortening this phrase to just Whether.

It is correct though if you mean 'regardless of whether'.

* fix(hook): explicitly specify the type

* feat(ci/build-binaries): cache the dependencies

* fix(hook): completely fix "Incomplete regular expression for hostnames"

CWE-20: Incomplete regular expression for hostnames

* feat(README): JOOX has been fixed :)

src/provider/kugou.js
@@ -5,8 +5,6 @@ const crypto = require('../crypto');
 const request = require('../request');
 
 const format = (song) => {
-	// const SingerName = song.SingerName.split('、')
-	const singername = song.singername.split('、');
 	return {
 		// id: song.FileHash,
 		// name: song.SongName,
@@ -42,23 +40,26 @@ const search = (info) => {
 };
 
 const single = (song, format) => {
-	// const url =
-	// 	'http://m.kugou.com/app/i/getSongInfo.php?cmd=playInfo&hash=' + id
-
-	// return request('GET', url)
-	// .then(response => response.json())
-	// .then(jsonBody => jsonBody.url || Promise.reject())
-
-	if (format === 'hash') id = song.id;
-	else if (format === 'hqhash') id = song.id_hq;
-	else if (format === 'sqhash') id = song.id_sq;
+	const getHashId = () => {
+		switch (format) {
+			case 'hash':
+				return song.id;
+			case 'hqhash':
+				return song.id_hq;
+			case 'sqhash':
+				return song.id_sq;
+			default:
+				break;
+		}
+		return '';
+	};
 
 	const url =
 		'http://trackercdn.kugou.com/i/v2/?' +
 		'key=' +
 		crypto.md5.digest(`${id}kgcloudv2`) +
 		'&hash=' +
-		id +
+		getHashId() +
 		'&' +
 		'appid=1005&pid=2&cmd=25&behavior=play&album_id=' +
 		song.album.id;

.editorconfig
@@ -9,28 +9,6 @@ charset = utf-8
 trim_trailing_whitespace = true
 insert_final_newline = true
 
-# Use 4 spaces for the Python files
-[*.py]
-indent_size = 4
-max_line_length = 80
-
-# The JSON files contain newlines inconsistently
-[*.json]
-insert_final_newline = ignore
-
-# Minified JavaScript files shouldn't be changed
-[**.min.js]
-indent_style = ignore
-insert_final_newline = ignore
-
-# Makefiles always use tabs for indentation
-[Makefile]
-indent_style = tab
-
-# Batch files use tabs for indentation
-[*.bat]
-indent_style = tab
-
 [*.md]
 trim_trailing_whitespace = false
 

.github/workflows/build-binaries.yml
@@ -34,6 +34,7 @@ jobs:
               uses: actions/setup-node@v2
               with:
                   node-version: 14
+                  cache: yarn
 
             - name: Set up QEMU
               id: qemu

src/cli.js
@@ -18,7 +18,7 @@ const cli = {
 		// metavar - A name for the argument in usage messages.
 		// help - A brief description of what the argument does.
 
-		// required - Whether or not the command-line option may be omitted (optionals only).
+		// required - Whether the command-line option may be omitted (optionals only).
 		// default - The value produced if the argument is absent from the command line.
 		// type - The type to which the command-line argument should be converted.
 		// choices - A container of the allowable values for the argument.
@@ -80,7 +80,7 @@ const cli = {
 			const index = part.startsWith('-')
 				? optionals[part]
 				: positionals.shift();
-			if (index == undefined)
+			if (index === undefined)
 				part.startsWith('-')
 					? error(`no such option: ${part}`)
 					: error(`extra arguments found: ${part}`);
@@ -172,7 +172,8 @@ const help = () => {
 		.map((option) => {
 			const { flags } = option;
 			const name = option.metavar || option.dest;
-			let use = '';
+			/** @type {string} */
+			let use;
 			if (
 				['store_true', 'store_false', 'help', 'version'].includes(
 					option.action

src/hook.js
@@ -104,8 +104,8 @@ hook.request.before = (ctx) => {
 		[url.hostname, req.headers.host].some((host) =>
 			hook.target.host.has(host)
 		) &&
-		req.method == 'POST' &&
-		(url.path == '/api/linux/forward' || url.path.startsWith('/eapi/'))
+		req.method === 'POST' &&
+		(url.path === '/api/linux/forward' || url.path.startsWith('/eapi/'))
 	) {
 		return request
 			.read(req)
@@ -116,10 +116,10 @@ hook.request.before = (ctx) => {
 				req.headers['X-Real-IP'] = '118.88.88.88';
 				if (req.url.includes('stream')) return; // look living eapi can not be decrypted
 				if (body) {
-					let data = null;
+					let data;
 					const netease = {};
 					netease.pad = (body.match(/%0+$/) || [''])[0];
-					netease.forward = url.path == '/api/linux/forward';
+					netease.forward = url.path === '/api/linux/forward';
 					if (netease.forward) {
 						data = JSON.parse(
 							crypto.linuxapi
@@ -156,7 +156,7 @@ hook.request.before = (ctx) => {
 					ctx.netease = netease;
 					// console.log(netease.path, netease.param)
 
-					if (netease.path == '/api/song/enhance/download/url')
+					if (netease.path === '/api/song/enhance/download/url')
 						return pretendPlay(ctx);
 				}
 			})
@@ -170,7 +170,8 @@ hook.request.before = (ctx) => {
 			web: true,
 			path: url.path
 				.replace(/^\/weapi\//, '/api/')
-				.replace(/\?.+$/, '')
+				.split('?')
+				.shift() // remove the query parameters
 				.replace(/\/\d*$/, ''),
 		};
 	} else if (req.url.includes('package')) {
@@ -194,7 +195,7 @@ hook.request.before = (ctx) => {
 };
 
 hook.request.after = (ctx) => {
-	const { req, proxyRes, netease, package } = ctx;
+	const { req, proxyRes, netease, package: pkg } = ctx;
 	if (
 		req.headers.host === 'tyst.migu.cn' &&
 		proxyRes.headers['content-range'] &&
@@ -204,7 +205,7 @@ hook.request.after = (ctx) => {
 	if (
 		netease &&
 		hook.target.path.has(netease.path) &&
-		proxyRes.statusCode == 200
+		proxyRes.statusCode === 200
 	) {
 		return request
 			.read(proxyRes, true)
@@ -233,7 +234,7 @@ hook.request.after = (ctx) => {
 				) {
 					if (netease.path.includes('manipulate'))
 						return tryCollect(ctx);
-					else if (netease.path == '/api/song/like')
+					else if (netease.path === '/api/song/like')
 						return tryLike(ctx);
 				} else if (netease.path.includes('url')) return tryMatch(ctx);
 				else if (netease.path.includes('/usertool/sound/'))
@@ -263,9 +264,9 @@ hook.request.after = (ctx) => {
 							value['st'] = 0;
 							value['subp'] = 1;
 							value['pl'] =
-								value['pl'] == 0 ? 320000 : value['pl'];
+								value['pl'] === 0 ? 320000 : value['pl'];
 							value['dl'] =
-								value['dl'] == 0 ? 320000 : value['dl'];
+								value['dl'] === 0 ? 320000 : value['dl'];
 						}
 					}
 					return value;
@@ -281,14 +282,14 @@ hook.request.after = (ctx) => {
 					: body;
 			})
 			.catch((error) => (error ? console.log(error, req.url) : null));
-	} else if (package) {
+	} else if (pkg) {
 		if (new Set([201, 301, 302, 303, 307, 308]).has(proxyRes.statusCode)) {
 			return request(
 				req.method,
 				parse(req.url).resolve(proxyRes.headers.location),
 				req.headers
 			).then((response) => (ctx.proxyRes = response));
-		} else if (/p\d+c*.music.126.net/.test(req.url)) {
+		} else if (/p\d+c*\.music\.126\.net/.test(req.url)) {
 			proxyRes.headers['content-type'] = 'audio/*';
 		}
 	}
@@ -302,7 +303,7 @@ hook.connect.before = (ctx) => {
 			hook.target.host.has(host)
 		)
 	) {
-		if (url.port == 80) {
+		if (url.port === 80) {
 			req.url = `${global.address || 'localhost'}:${global.port[0]}`;
 			req.local = true;
 		} else if (global.port[1]) {
@@ -328,7 +329,7 @@ hook.negotiate.before = (ctx) => {
 const pretendPlay = (ctx) => {
 	const { req, netease } = ctx;
 	const turn = 'http://music.163.com/api/song/enhance/player/url';
-	let query = null;
+	let query;
 	if (netease.forward) {
 		const { id, br } = netease.param;
 		netease.param = { ids: `["${id}"]`, br };
@@ -399,14 +400,15 @@ const computeHash = (task) =>
 const tryMatch = (ctx) => {
 	const { req, netease } = ctx;
 	const { jsonBody } = netease;
-	let tasks = [],
-		target = 0;
+	/** @type {Promise<any>[]} */
+	let tasks;
+	let target = 0;
 
 	const inject = (item) => {
 		item.flag = 0;
 		if (
-			(item.code != 200 || item.freeTrialInfo) &&
-			(target == 0 || item.id == target)
+			(item.code !== 200 || item.freeTrialInfo) &&
+			(target === 0 || item.id === target)
 		) {
 			return match(item.id)
 				.then((song) => {
@@ -468,7 +470,7 @@ const tryMatch = (ctx) => {
 										  ),
 								[]
 							)
-							.filter((pair) => pair[0] != pair[1])[0];
+							.filter((pair) => pair[0] !== pair[1])[0];
 						return !difference || difference[0] <= difference[1];
 					};
 					const limit = { android: '0.0.0', osx: '0.0.0' };
@@ -500,7 +502,7 @@ const tryMatch = (ctx) => {
 					} catch (e) {}
 				})
 				.catch(() => {});
-		} else if (item.code == 200 && netease.web) {
+		} else if (item.code === 200 && netease.web) {
 			item.url = item.url.replace(
 				/(m\d+?)(?!c)\.music\.126\.net/,
 				'$1c.music.126.net'

src/kwDES.js
@@ -6,29 +6,30 @@
 
 const Long =
 	typeof BigInt === 'function' // BigInt support in Node 10+
-		? (n) => (
-				(n = BigInt(n)),
-				{
-					low: Number(n),
-					valueOf: () => n.valueOf(),
-					toString: () => n.toString(),
-					not: () => Long(~n),
-					isNegative: () => n < 0,
-					or: (x) => Long(n | BigInt(x)),
-					and: (x) => Long(n & BigInt(x)),
-					xor: (x) => Long(n ^ BigInt(x)),
-					equals: (x) => n === BigInt(x),
-					multiply: (x) => Long(n * BigInt(x)),
-					shiftLeft: (x) => Long(n << BigInt(x)),
-					shiftRight: (x) => Long(n >> BigInt(x)),
-				}
-		  )
+		? (n) => {
+				const bN = BigInt(n);
+
+				return {
+					low: Number(bN),
+					valueOf: () => bN.valueOf(),
+					toString: () => bN.toString(),
+					not: () => Long(~bN),
+					isNegative: () => bN < 0,
+					or: (x) => Long(bN | BigInt(x)),
+					and: (x) => Long(bN & BigInt(x)),
+					xor: (x) => Long(bN ^ BigInt(x)),
+					equals: (x) => bN === BigInt(x),
+					multiply: (x) => Long(bN * BigInt(x)),
+					shiftLeft: (x) => Long(bN << BigInt(x)),
+					shiftRight: (x) => Long(bN >> BigInt(x)),
+				};
+		  }
 		: (...args) => new (require('long'))(...args);
 
 const range = (n) => Array.from(new Array(n).keys());
 const power = (base, index) =>
 	Array(index)
-		.fill()
+		.fill(null)
 		.reduce((result) => result.multiply(base), Long(1));
 const LongArray = (...array) =>
 	array.map((n) => (n === -1 ? Long(-1, -1) : Long(n)));
@@ -465,25 +466,23 @@ const bitTransform = (arrInt, n, l) => {
 };
 
 const DES64 = (longs, l) => {
-	// long[], long
-	let out = Long(0);
-	let SOut = Long(0);
 	const pR = range(8).map(() => Long(0));
 	const pSource = [Long(0), Long(0)];
 	let L = Long(0);
 	let R = Long(0);
-	out = bitTransform(arrayIP, 64, l);
+	let out = bitTransform(arrayIP, 64, l);
 	pSource[0] = out.and(0xffffffff);
 	pSource[1] = out.and(-4294967296).shiftRight(32);
 
 	range(16).forEach((i) => {
+		let SOut = Long(0);
+
 		R = Long(pSource[1]);
 		R = bitTransform(arrayE, 64, R);
 		R = R.xor(longs[i]);
 		range(8).forEach((j) => {
 			pR[j] = R.shiftRight(j * 8).and(255);
 		});
-		SOut = Long(0);
 		range(8)
 			.reverse()
 			.forEach((sbi) => {

src/provider/find.js
@@ -12,9 +12,12 @@ const filter = (object, keys) =>
 const limit = (text) => {
 	const output = [text[0]];
 	const length = () => output.reduce((sum, token) => sum + token.length, 0);
-	text.slice(1).some((token) =>
-		length() > 15 ? true : (output.push(token), false)
-	);
+	text.slice(1).some((token) => {
+		if (length() > 15) return true;
+
+		output.push(token);
+		return false;
+	});
 	return output;
 };
 
@@ -23,9 +26,9 @@ const getFormatData = (data) => {
 		const info = filter(data, ['id', 'name', 'alias', 'duration']);
 		info.name = (info.name || '')
 			.replace(/（\s*cover[:：\s][^）]+）/i, '')
-			.replace(/\(\s*cover[:：\s][^\)]+\)/i, '')
+			.replace(/\(\s*cover[:：\s][^)]+\)/i, '')
 			.replace(/（\s*翻自[:：\s][^）]+）/, '')
-			.replace(/\(\s*翻自[:：\s][^\)]+\)/, '');
+			.replace(/\(\s*翻自[:：\s][^)]+\)/, '');
 		info.album = filter(data.album, ['id', 'name']);
 		info.artists = data.artists.map((artist) =>
 			filter(artist, ['id', 'name'])

src/provider/match.js
@@ -3,6 +3,7 @@ require('../polyfills');
 const find = require('./find');
 const request = require('../request');
 const consts = require('../consts');
+const { isHostWrapper } = require('../utilities');
 const providers = consts.PROVIDERS;
 const defaultSrc = consts.DEFAULT_SOURCE;
 
@@ -46,12 +47,12 @@ const match = (id, source, data) => {
 const check = (url) => {
 	const song = { size: 0, br: null, url: null, md5: null };
 	let header = { range: 'bytes=0-8191' };
-	if (url.includes('bilivideo.com')) {
-		header = {
-			range: 'bytes=0-8191',
-			referer: 'https://www.bilibili.com/',
-		};
+	const isHost = isHostWrapper(url);
+
+	if (isHost('bilivideo.com')) {
+		header.referer = 'https://www.bilibili.com/';
 	}
+
 	return Promise.race([
 		request('GET', url, header),
 		new Promise((_, reject) => setTimeout(() => reject(504), 5 * 1000)),
@@ -59,10 +60,10 @@ const check = (url) => {
 		.then((response) => {
 			if (!response.statusCode.toString().startsWith('2'))
 				return Promise.reject();
-			if (url.includes('126.net'))
+			if (isHost('126.net'))
 				// song.md5 = response.headers['x-nos-meta-origin-md5'] || response.headers['etag'].replace(/"/g, '')
 				song.md5 = url.split('/').slice(-1)[0].replace(/\..*/g, '');
-			else if (url.includes('qq.com'))
+			else if (isHost('qq.com'))
 				song.md5 = response.headers['server-md5'];
 			else if (url.includes('qianqian.com'))
 				song.md5 = response.headers['etag']

commit 26ad320858e598dc1a70387c05503a412f9078d1

fix(provider): kugou: update songId var
This resolves one of the errors mentioned in #118.

Fixes: 6e06698d0649170 ("refactor: Resolve a lot of warning from
 CodeQL and WebStorm")

Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

src/provider/kugou.js
@@ -57,7 +57,7 @@ const single = (song, format) => {
 	const url =
 		'http://trackercdn.kugou.com/i/v2/?' +
 		'key=' +
-		crypto.md5.digest(`${id}kgcloudv2`) +
+		crypto.md5.digest(`${getHashId()}kgcloudv2`) +
 		'&hash=' +
 		getHashId() +
 		'&' +