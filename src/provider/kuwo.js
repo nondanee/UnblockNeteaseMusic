36ce09224942d3ea846bbb4182a7ab3318e73251
Tianling Shen <cnsztl@gmail.com>
2021-08-23 14:31:48 +0800

    fix(provider/kuwo): check status code before formatting info (#121)
    This fixes the following error:
    
    ```
    TypeError: Cannot read property 'list' of undefined
        at request.then.then.then.then (/UnblockNeteaseMusic/src/provider/kuwo.js:67:31)
        at process._tickCallback (internal/process/next_tick.js:68:7)
    ```
    
    Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

--- src/provider/kuwo.js
+++ src/provider/kuwo.js
@@ -63,6 +63,8 @@ const search = (info) => {
 		)
 		.then((response) => response.json())
 		.then((jsonBody) => {
+			if (jsonBody && typeof jsonBody === "object" && "code" in jsonBody && jsonBody.code !== 200)
+				return Promise.reject();
 			const list = jsonBody.data.list.map(format);
 			const matched = select(list, info);
 			return matched ? matched.id : Promise.reject();
