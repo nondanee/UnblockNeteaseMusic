14ba3777ebe4a3cf58a88e72dace074cb40bdbec
Yi-Jyun Pan <pan93412@gmail.com>
2021-09-12 13:36:45 +0800

    feat: add the bundler for future use

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import json from '@rollup/plugin-json';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+
+const commonConfig = {
+	output: {
+		dir: 'precompiled',
+		format: 'cjs',
+		banner: ['#!/usr/bin/node', ''].join('\n'),
+	},
+	plugins: [
+		commonjs(),
+		json(),
+		babel({
+			babelHelpers: 'bundled',
+		}),
+		nodeResolve(),
+	],
+};
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const appConfig = {
+	input: './src/app.js',
+	...commonConfig,
+};
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const bridgeConfig = {
+	input: './src/bridge.js',
+	...commonConfig,
+};
+
+export default [appConfig, bridgeConfig];
