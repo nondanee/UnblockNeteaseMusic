d1919dc9864980a751aaa999bc75eefdb40c6a03
Yi-Jyun Pan <pan93412@gmail.com>
2021-09-14 22:25:05 +0800

    fix(rollup): Replace `readable-stream` to `stream.Transform` as a workaround to the circular dependency issue

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
 
 /**
  * @type {Partial<import('rollup').RollupOptions>}
@@ -18,7 +19,23 @@ const commonConfig = {
 		babel({
 			babelHelpers: 'bundled',
 		}),
-		nodeResolve(),
+		nodeResolve({
+			preferBuiltins: true,
+		}),
+		replace({
+			preventAssignment: true,
+			delimiters: ['', ''],
+			values: {
+				// Replace `readable-stream` to `stream.Transform` as
+				// a workaround to the circular dependency issue.
+				// https://github.com/rollup/rollup/issues/1507#issuecomment-340550539
+				"require('readable-stream/transform')":
+					"require('stream').Transform",
+				'require("readable-stream/transform")':
+					'require("stream").Transform',
+				'readable-stream': 'stream',
+			},
+		}),
 	],
 };
 
