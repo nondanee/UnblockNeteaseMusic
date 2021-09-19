6e70bcb29a7fa6324fe643823da476dc4be51b5f
Tianling Shen <i@cnsztl.eu.org>
2021-09-20 03:37:56 +0800

    fix(request): make sure custom headers won't be overwritten
    Fixes: #174
    
    Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

--- src/request.js
+++ src/request.js
@@ -90,13 +90,13 @@ const request = (
 		method,
 		url,
 		{
-			...headers,
 			host: url.hostname,
 			accept: 'application/json, text/plain, */*',
 			'accept-encoding': 'gzip, deflate',
 			'accept-language': 'zh-CN,zh;q=0.9',
 			'user-agent':
 				'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+			...headers,
 		},
 		proxy
 	);
